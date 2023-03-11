cbd3f1615f5e554ea76177a789a760e5f5f6e8d0
Eugene Koontz <ekoontz@hiro-tan.org>
2023-03-11 19:37:12 +0100

    upgrade slj4j to newer release

--- project.clj
+++ project.clj
@@ -15,9 +15,8 @@
   
   :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.2.17"]
                                   [com.bhauman/rebel-readline-cljs "0.1.4"]
-                                  ;; to silence SL4J warnings
-                                  [org.slf4j/slf4j-log4j12 "1.7.30"] 
-                                  ]}}
+                                  [org.slf4j/slf4j-reload4j "2.0.6"]]}}
+
 
   :aliases {"fig"       ["trampoline" "run" "-m" "figwheel.main"]
             "fig:build" ["trampoline" "run" "-m" "figwheel.main" "-b" "flappy" "-r"]})
