db93eee288bb30ffe94624fa8a0f3564816a046b
Eugene Koontz <ekoontz@hiro-tan.org>
2023-03-11 19:07:02 +0100

    add more documentation

--- src/flappy_bird_demo/log.cljs
+++ src/flappy_bird_demo/log.cljs
@@ -1,5 +1,8 @@
 (ns ekoontz.log)
 
+;; thanks to all the commenters on
+;; https://stackoverflow.com/questions/24239144/js-console-log-in-clojurescript
+
 (defn info [& args]
   (.apply js/console.info js/console (to-array args)))
 
@@ -11,3 +14,30 @@
 
 (defn warn [& args]
   (.apply js/console.warn js/console (to-array args)))
+
+
+;; another possible set of implementations: (not sure which is better):
+
+(comment
+
+(defn fmt [msgs]
+  (apply str (interpose " " (map pr-str msgs))))
+
+(defn debug [& s]
+  (let [msg (fmt s)]
+    (js/console.debug msg)))
+
+(defn error [& s]
+  (let [msg (fmt s)]
+    (js/console.error msg)))
+
+(defn info [& s]
+  (let [msg (fmt s)]
+    (js/console.info msg)))
+
+(defn warn [& s]
+  (let [msg (fmt s)]
+    (js/console.warn msg)))
+
+)
+
