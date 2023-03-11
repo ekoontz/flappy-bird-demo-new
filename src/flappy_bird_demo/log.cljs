f5697187efe0c3b0a6cb11f199088e1af1fe69a7
Eugene Koontz <ekoontz@hiro-tan.org>
2023-03-11 17:46:36 +0100

    add more logging levels

--- src/flappy_bird_demo/log.cljs
+++ src/flappy_bird_demo/log.cljs
@@ -2,3 +2,12 @@
 
 (defn info [& args]
   (.apply js/console.info js/console (to-array args)))
+
+(defn debug [& args]
+  (.apply js/console.debug js/console (to-array args)))
+
+(defn error [& args]
+  (.apply js/console.error js/console (to-array args)))
+
+(defn warn [& args]
+  (.apply js/console.warn js/console (to-array args)))
